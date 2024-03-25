7094f642955432d73461975b8de9130a92a912d4
Da-Wei Lee <daviddwlee84@gmail.com>
2024-03-25 11:14:04 +0800

    Able to load OpenAPI yaml

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # LLM-API-Agent
+
 Creating tools for LLM so it can call API by itself and finish complex tasks
+
+## Getting Started
+
+Python 3.10+
+
+## Resources
+
+### LangChain
+
+Tools and Agent (general)
+
+- [Agents | 🦜️🔗 Langchain](https://python.langchain.com/docs/modules/agents/)
+  - [Tools | 🦜️🔗 Langchain](https://python.langchain.com/docs/modules/agents/tools/)
+- [Choosing between multiple tools | 🦜️🔗 Langchain](https://python.langchain.com/docs/use_cases/tool_use/multiple_tools)
+- [Agents | 🦜️🔗 Langchain](https://python.langchain.com/docs/use_cases/tool_use/agents)
+
+API Agent and Planner
+
+- [OpenAPI | 🦜️🔗 Langchain](https://python.langchain.com/docs/integrations/toolkits/openapi)
+
+### Papers
+
+Agents/Tools (general)
+
+- [[2210.03629] ReAct: Synergizing Reasoning and Acting in Language Models](https://arxiv.org/abs/2210.03629)
+
+API Agent (specific)
+
+- [[2306.06624] RestGPT: Connecting Large Language Models with Real-World RESTful APIs](https://arxiv.org/abs/2306.06624)
+
+### API Documents
+
+- [APIs-guru/openapi-directory: 🌐 Wikipedia for Web APIs. Directory of REST API definitions in OpenAPI 2.0/3.x format](https://github.com/APIs-guru/openapi-directory)
+- [APIs.guru](https://apis.guru/)
---
+++ utils/api.py
@@ -0,0 +1,103 @@
+from langchain_community.agent_toolkits.openapi.spec import reduce_openapi_spec, ReducedOpenAPISpec
+import tiktoken
+import requests
+import yaml
+from urllib.parse import urlparse
+import os
+
+
+class OpenAPIManager:
+    """
+    https://python.langchain.com/docs/integrations/toolkits/openapi
+    """
+
+    _spec: dict | list | ReducedOpenAPISpec
+    _spec_path: str
+
+    def __init__(self, yaml_url_or_path: str | None = None, default_model: str = 'gpt-4', reduce_spec: bool = True):
+        self.encoder = tiktoken.encoding_for_model(default_model)
+        self._to_reduce_spec = reduce_spec
+        self._spec_path = yaml_url_or_path
+
+        if yaml_url_or_path is not None:
+            if self.is_local(yaml_url_or_path):
+                self._spec = self.load_local_yaml(yaml_url_or_path)
+            else:
+                self._spec = self.load_online_yaml(yaml_url_or_path)
+
+    @staticmethod
+    def is_local(url_or_path: str) -> bool:
+        """
+        https://stackoverflow.com/questions/68626097/pythonic-way-to-identify-a-local-file-or-a-url
+        """
+        url_parsed = urlparse(url_or_path)
+        if url_parsed.scheme in ('file', ''): # Possibly a local file
+            return os.exists(url_parsed.path)
+        return False
+
+    @property
+    def spec_tokens(self):
+        def count_tokens(string: str):
+            """
+            *** ValueError: Encountered text corresponding to disallowed special token '<|endoftext|>'.
+            https://github.com/langchain-ai/langchain/issues/923
+            https://github.com/langchain-ai/langchain/pull/924
+            """
+            return len(self.encoder.encode(string, disallowed_special=()))
+        return count_tokens(yaml.dump(self._spec))
+
+    @property
+    def spec_endpoints(self) -> int:
+        if not self._to_reduce_spec:
+            endpoints = [
+                (route, operation)
+                for route, operations in self._spec["paths"].items()
+                for operation in operations
+                if operation in ["get", "post"]
+            ]
+        else:
+            endpoints = self._spec.endpoints
+        return len(endpoints)
+
+    def load_online_yaml(self, yaml_url: str, update_spec: bool = True) -> dict | list | ReducedOpenAPISpec:
+        """
+        https://stackoverflow.com/questions/65191010/how-to-load-a-yaml-file-from-url-to-process-in-python
+        """
+        response = requests.get(yaml_url, allow_redirects=True)
+        api_spec = yaml.safe_load(response.content.decode('utf-8'))
+
+        if self._to_reduce_spec:
+            api_spec = reduce_openapi_spec(api_spec)
+        
+        if update_spec:
+            self._spec = api_spec
+            self._spec_path = yaml_url
+        return api_spec
+    
+    def load_local_yaml(self, yaml_path: str, update_spec: bool = True) -> dict | list | ReducedOpenAPISpec:
+        with open(yaml_path) as f:
+            api_spec = yaml.load(f, Loader=yaml.Loader)
+        
+        if self._to_reduce_spec:
+            api_spec = reduce_openapi_spec(api_spec)
+    
+        if update_spec:
+            self._spec = api_spec
+            self._spec_path = yaml_path
+        return api_spec
+        
+
+if __name__ == '__main__':
+    manager = OpenAPIManager("https://raw.githubusercontent.com/APIs-guru/openapi-directory/main/APIs/spotify.com/1.0.0/openapi.yaml")
+    print(manager._spec)
+    print(type(manager._spec))
+    print(manager.spec_tokens)
+    print(manager.spec_endpoints)
+    import ipdb; ipdb.set_trace()
+    manager2 = OpenAPIManager(reduce_spec=False)
+    manager2.load_online_yaml("https://raw.githubusercontent.com/openai/openai-openapi/master/openapi.yaml")
+    print(manager2._spec)
+    print(type(manager2._spec))
+    print(manager2.spec_tokens)
+    print(manager2.spec_endpoints)
+    import ipdb; ipdb.set_trace()
